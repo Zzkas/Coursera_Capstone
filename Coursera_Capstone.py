c52f04e532ae9ba4ab3be6cad2e87134d710bcdf
Zzkas <56826640+Zzkas@users.noreply.github.com>
2019-12-18 00:10:42 +0100

    Add files via upload
    Assignment: Capstone Project Notebook

---
+++ Coursera_Capstone.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #  Coursera Capstone Project Notebook - Battle of The Neighborhoods 
+# 
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[5]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
